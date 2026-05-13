90f40903d74443109151a5d73c2d173aad06a294
Tanishka Raghuwanshi <tanishka.raghuwanshi09@gmail.com>
2026-05-13 23:09:20 +0530

    Two sum

--- 1.cpp
+++ 1.cpp
@@ -17,3 +17,23 @@ public:
     }
 
 };
+
+// optimized 
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int,int> mp;
+
+//         for(int i=0;i<nums.size();i++){
+//             int sum=target - nums[i];
+
+//             if(mp.find(sum) !=mp.end()){
+//                 return {mp[sum],i};
+//             }
+//             mp[nums[i]]=i;
+//         } 
+//         return {};
+        
+//     }
+// };
