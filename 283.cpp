4af49f3d382d54cfa743a4ef9b5e9cda3dc84a72
Tanishka Raghuwanshi <tanishka.raghuwanshi09@gmail.com>
2026-05-16 22:17:33 +0530

    Move Zeroes--two pointer

---
+++ 283.cpp
@@ -0,0 +1,18 @@
+// Move Zeroes
+// TWO POINTER
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]!=0){
+            swap(nums[i],nums[j]);
+            j++;
+        }
+
+       }
+
+        
+    }
+};
