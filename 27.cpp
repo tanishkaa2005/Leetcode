8a637f41e063e610c8f6c121c8da922d1f75e69f
Tanishka Raghuwanshi <tanishka.raghuwanshi09@gmail.com>
2026-05-16 18:12:00 +0530

    Remove Element--two pointer

---
+++ 27.cpp
@@ -0,0 +1,21 @@
+Remove Element
+// TWO POINTER
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+                
+                
+            }
+           
+        
+        }
+        return j;
+        
+    }
+};
